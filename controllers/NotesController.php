22d538b95a7a2c071b502ea683a5edfd6ca94945
victor braz <victor.90braz@gmail.com>
2023-09-15 17:20:28 +0200

    reorder sequence

--- controllers/NotesController.php
+++ controllers/NotesController.php
@@ -1,10 +1,9 @@
 <?php
+include __DIR__ . "/../views/notes.php";
 
 $config = require __DIR__ . "/../config/config.php";
-
 $dataBase = new DataBase($config["dataBase"]);
-$notes = $dataBase->query("SELECT * from notes")->fetchAll();
 
+$notes = $dataBase->query("SELECT * from notes")->fetchAll();
 printArray($notes);
 
-include __DIR__ . "/../views/notes.php";
