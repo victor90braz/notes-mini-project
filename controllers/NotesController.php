9da86fa5943d3af8fca79c93ac2bbfc8473a24e6
victor braz <victor.90braz@gmail.com>
2023-09-15 17:19:36 +0200

    add create notes to notescontroller

--- controllers/NotesController.php
+++ controllers/NotesController.php
@@ -4,6 +4,7 @@ $config = require __DIR__ . "/../config/config.php";
 
 $dataBase = new DataBase($config["dataBase"]);
 $notes = $dataBase->query("SELECT * from notes")->fetchAll();
+
 printArray($notes);
 
 include __DIR__ . "/../views/notes.php";
