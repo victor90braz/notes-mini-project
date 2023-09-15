2eb053b3b690a844b06f123446765d7322d34ef1
victor braz <victor.90braz@gmail.com>
2023-09-15 18:25:07 +0200

    add note to nav

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,6 +3,6 @@
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/notes">Notes</a></li>
-        <li><a href="/note">Note</a></li>
+        <li><a href="/note?id=<?= $note['id']?>">Note</a></li>
     </ul>
 </nav>
