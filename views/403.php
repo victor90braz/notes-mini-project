51176795b83841e707792e18ac80555af6cd91b6
victor braz <victor.90braz@gmail.com>
2023-09-17 10:49:09 +0200

    create errors feedbacks

--- views/403.php
+++ views/403.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/partials/banner.php'; ?>
 
 <div class="bg-white p-8 rounded-lg shadow-lg text-center">
-    <h1 class="text-4xl font-semibold text-gray-800">Unauthorized</h1>
+    <h1>You are not authorized to view this page.</h1>
     <a href="/" class="text-blue-500 mt-4 hover:underline">Go back to the homepage</a>
 </div>
 
