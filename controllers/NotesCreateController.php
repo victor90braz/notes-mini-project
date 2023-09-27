7bd71a248e5398d2c06f5ad74937cd1174f84c0e
victor braz <victor.90braz@gmail.com>
2023-09-27 15:52:02 +0200

    refactor create note

--- controllers/NotesCreateController.php
+++ controllers/NotesCreateController.php
@@ -2,7 +2,19 @@
 $config = require __DIR__ . "/../config/config.php";
 $dataBase = new DataBase($config["dataBase"]);
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $noteBody = $_POST["body"];
+    $userId = 1;
 
+    $query = "INSERT INTO notes (body, user_id) VALUES (:body, :user_id)";
+    $params = [":body" => $noteBody, ":user_id" => $userId];
+
+    try {
+        $dataBase->query($query, $params);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();  // Handle the error appropriately
+    }
+}
 
 include __DIR__ . "/../views/noteCreate.php";
 ?>
