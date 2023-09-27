9b3cac8b6ecb76c94f659b9aadf3e0b784e9c38b
victor braz <victor.90braz@gmail.com>
2023-09-27 14:36:52 +0200

    add style to the form

--- views/noteCreate.php
+++ views/noteCreate.php
@@ -1,10 +1,9 @@
 <?php include __DIR__ . "/partials/banner.php" ?>
 <?php include __DIR__ . '\partials\nav.php' ?>
 
-
 <main>
   <div class="container-form">
-    <form action="" method="POST">
+    <form method="POST">
       <label for="body">Note</label>
       <textarea name="body" id="body" cols="30" rows="10"></textarea>
       <button type="submit">Create</button>
