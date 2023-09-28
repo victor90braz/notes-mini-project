6eb7e2af58fe7c3eadcc402c007665a1096d736e
victor braz <victor.90braz@gmail.com>
2023-09-28 14:39:56 +0200

    send server side feedback

--- views/noteCreate.php
+++ views/noteCreate.php
@@ -6,6 +6,10 @@
     <form method="POST">
       <label for="body">Note</label>
       <textarea name="body" id="body" cols="30" rows="10"></textarea>
+      <?php
+        if (isset($errors['body'])) : ?>
+          <p><?= $errors['body'] ?></p>
+      <?php endif; ?>
       <button type="submit">Create</button>
     </form>
   </div>
